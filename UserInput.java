9e017101adb60593bc1757e216cc6fba3502ff4d
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-02-03 22:58:30 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -1,16 +1,26 @@
-//UserInput.java
+/*
+ * Name: Rithikaa Redde
+ * PRN: <Your PRN>
+ * Batch: <Your Batch>
+ */
 
 import java.util.*;
-public class UserInput{
-    int[] arrayInput(){
+
+public class UserInput {
+
+    // Method to take input from the user and return an array of numbers
+    int[] arrayInput() {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
+        int[] arr = new int[5]; // Define the array size (5)
+        
         System.out.println("Enter 5 numbers: ");
-        for(int i = 0; i < arr.length; i++)
-        {
-            arr[i]= sc.nextInt();
+        
+        // Taking 5 numbers from the user and storing them in the array
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        return(arr);
-        //This is a comment
+        
+        // Returning the array
+        return arr;
     }
 }

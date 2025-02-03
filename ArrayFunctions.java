9b32263b39dca5389b1478abaca3d435ff7ef60a
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-02-03 22:57:57 +0530

    Update ArrayFunctions.java

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -1,32 +1,37 @@
+/*
+ * Name: Rithikaa Redde
+ * PRN: <Your PRN>
+ * Batch: <Your Batch>
+ */
+
 import java.util.*;
-public class ArrayFunctions{
-    public void display(ArrayList<Integer> arr)
-    {
-        System.out.println("Array: "+arr);
+
+public class ArrayFunctions {
+    
+    // Method to display the array content
+    public void display(ArrayList<Integer> arr) {
+        System.out.println("Array: " + arr);
     }
-    public void oddEven(int[] arr)
-    {
+    
+    // Method to categorize numbers into odd and even arrays
+    public void oddEven(int[] arr) {
+        // Initializing ArrayLists for odd and even numbers
         ArrayList<Integer> odd = new ArrayList<Integer>();
         ArrayList<Integer> eve = new ArrayList<Integer>();
-        int odd_index = 0, even_index = 0;
-        int i=0;
-        for(i=0; i<arr.length; i++)
-        {
-            if(arr[i]%2 == 0)
-            {
-                eve.add(arr[i]);
-                even_index++;
-            }
-            else
-            {
-                odd.add(arr[i]);
-                odd_index++;
+        
+        // Iterating through the input array
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 0) {
+                eve.add(arr[i]); // Add to even array
+            } else {
+                odd.add(arr[i]); // Add to odd array
             }
         }
-        System.out.print("ODD NUMBERS ");
+        
+        // Displaying the categorized numbers
+        System.out.print("ODD NUMBERS: ");
         display(odd);
         System.out.print("EVEN NUMBERS: ");
         display(eve);
-
     }
 }
